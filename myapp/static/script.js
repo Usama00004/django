56766209d5f1226b2a31c501406d9ab2575c95d6
Usama Tahir <usamatahir00004@gmail.com>
2023-10-29 22:55:22 +0100

    first commit

---
+++ myapp/static/script.js
@@ -0,0 +1,56 @@
+// $(document).ready(function() {
+//     $('#dataForm').submit(function(event) {
+//         event.preventDefault();
+//         $.ajax({
+//             type: 'POST',
+//             url: '/submit/',
+//             data: {
+//                 name: $('#name').val(),
+//                 phone: $('#phone').val(),
+//                 csrfmiddlewaretoken: $('input[name=csrfmiddlewaretoken]').val()
+//             },
+//             success: function(response) {
+//                 alert(response.message);
+//             }
+//         });
+//     });
+// });
+
+
+$(document).ready(function() {
+    $('#dataForm').submit(function(event) {
+        event.preventDefault();
+        $.ajax({
+            type: 'POST',
+            url: '/submit/',
+            data: {
+                name: $('#name').val(),
+                phone: $('#phone').val(),
+                csrfmiddlewaretoken: $('input[name=csrfmiddlewaretoken]').val()
+            },
+            success: function(response) {
+                alert(response.message);
+            }
+        });
+    });
+
+    // Function to fetch data from the backend and display it
+    function fetchData() {
+        
+        $.ajax({
+            type: 'GET',
+            url: '/get_data/',
+            success: function(response) {
+                var users = response.users;
+                var output = '<h2>Fetched Data:</h2><ul>';
+                for (var i = 0; i < users.length; i++) {
+                    output += '<li>Name: ' + users[i].name + ', Phone: ' + users[i].phone + '</li>';
+                }
+                output += '</ul>';
+                $('#output').html(output);
+            }
+        });
+    }
+    // Fetch data when the page is loaded
+    fetchData();
+});
---
+++ myapp/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from .models import User
+
+# def index(request):
+#     users = User.objects.all()
+#     return render(request, 'index.html', {'users': users})
+
+
+def index(request):
+    return render(request, 'index.html')
+
+def submit_data(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        User.objects.create(name=name, phone=phone)
+        return JsonResponse({'message': 'Data submitted successfully!'})
+    return JsonResponse({'message': 'Invalid request!'})
+
+def get_data(request):
+    users = User.objects.all().values()  # Convert queryset to dictionary
+    return JsonResponse({'users': list(users)})
